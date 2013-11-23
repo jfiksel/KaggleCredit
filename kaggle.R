ee1be87609e74213c8cc10747e8241e6f01a1487
Bill DeRose <bill.derose11@gmail.com>
2013-11-22 19:33:44 -0800

    Simple data clean up; brute force removal

--- kaggle.R
+++ kaggle.R
@@ -4,30 +4,64 @@ library(ada)
 library(ggplot2)
 library(randomForest)
 
-## we should do some data clean up. There's a guy with age 0 that we should probably take out of the set entirely
-## when we look at cs.training$NumberOfTime30.59DaysPastDueNotWorse a value of 96/98 coded for some qualitative
-## quantity ("other", or "refused to say") and so we shoud clean these up because they skew the data. The same corruption
-## is present in cs.training$NumberOfTime60.89DaysPastDueNotWorse and it also looks to be present in NumberOfTimes90DaysLate
-##
-## should think of a way to weight observations where the person did default since these obs. are under
-## represented in the data.
+## Someone has age 0. We should probably take out of the set, unless we can come up 
+## good interpolation. Losing one observation isn't huge. NumberOfTime30.59DaysPastDueNotWorse 
+## contains "non-sense" data such as 96 and 98 use to code for some qualitative
+## quantity ("other", or "refused to say"). The same corruption is present in 
+## NumberOfTime60.89DaysPastDueNotWorse and NumberOfTimes90DaysLate
+## See tabulated values using the raw data from cs-training.csv
 table(cs.training$age)
 table(cs.training$NumberOfTime30.59DaysPastDueNotWorse)
 table(cs.training$NumberOfTimes90DaysLate)
+table(cs.training$NumberOfTime60.89DaysPastDueNotWorse)
 
-## Data imputation: we should impute all the data before we begin training/testing with it, 
-## so the sooner the better.
+## DATA CLEANUP TODO:
+## We should think about ways to weight observations where the person did 
+## default since defaulting obs. are underrepresented in the data.
+p1 <- ggplot(cs.training) + 
+      geom_bar(aes(x = factor(SeriousDlqin2yrs), y = ..count../sum(..count..))) + 
+      ylab("Percent") + xlab("Classification") +
+      scale_y_continuous(labels = percent_format())
+p1
 
-# split data in half and sample from it. Our training/test data is all pulled from cs.training, each
-# subset representing about a quarter of the data in total
-cs.training.half <-cs.training[sample(nrow(cs.training), nrow(cs.training)/2), ]
-sample.rows <- sample(nrow(cs.training.half), nrow(cs.training.half)/2) 
-train.sub <- na.omit(cs.training.half[sample.rows, 2:ncol(cs.training.half)])
-test.sub <- na.omit(cs.training.half[-sample.rows, 2:ncol(cs.training.half)])
+## We should impute all the data before we begin training/testing with it. 
+## Here is a naive attempt that removes the offending observations from our data.
+## We choose to omit NA values as well.
+temp <- cs.training[cs.training$age > 0, ] 
+temp <- temp[temp$NumberOfTime30.59DaysPastDueNotWorse < 20, ]
+temp <- temp[temp$NumberOfTimes90DaysLate < 90, ]
+temp <- temp[temp$NumberOfTime60.89DaysPastDueNotWorse < 90, ]
+naive.cleaned <- na.omit(temp)
+n <- nrow(naive.cleaned)
+half.cleaned <- naive.cleaned[sample(n, n/1.5), ]
+
+# split data in half and sample from it.
+train.rows <- sample(nrow(half.cleaned), floor(nrow(half.cleaned)*0.8))
+train.set <- half.cleaned[train.rows, 2:ncol(half.cleaned)]
+test.set <- half.cleaned[-train.rows, 2:ncol(half.cleaned)]
+
+## proof we actally removed offending samples
+table(train.set$age)
+table(train.set$NumberOfTime30.59DaysPastDueNotWorse)
+table(train.set$NumberOfTimes90DaysLate)
+table(train.set$NumberOfTime60.89DaysPastDueNotWorse)
+
+# let's check how defaults are distributed in these:
+p2 <- ggplot(half.cleaned) + 
+      geom_bar(aes(x = factor(SeriousDlqin2yrs), y = ..count../sum(..count..))) + 
+      ylab("Percent") + xlab("Classification") +
+      scale_y_continuous(labels = percent_format())
+p3 <- ggplot(test.set) + 
+        geom_bar(aes(x = factor(SeriousDlqin2yrs), y = ..count../sum(..count..))) + 
+        ylab("Percent") + xlab("Classification") +
+        scale_y_continuous(labels = percent_format())
+p2
+p3
+
 
 ### SVM: these take a long time too. The first call to tune.svm is a cross-validation step
 ### so that we are using the optimal parameters to train with
-if (false) {
+if (FALSE) {
   tuned <- tune.svm(as.factor(SeriousDlqin2yrs)~., data = train.sub,
                     gamma = 10^(-6:2), cost = 10^(1:3), kernel="polynomial")
 
@@ -42,7 +76,7 @@ if (false) {
 
 ### random forest
 # this will take a while
-rf.fit <- randomForest(SeriousDlqin2yrs~., data = train.sub, ntrees = 500)
+rf.fit <- randomForest(SeriousDlqin2yrs~., data = train.sub, ntrees = 100)
 
 ### Boosting: as of now we are using weak classifiers in our boosting routine and only running for
 ### 100 iterations. We may want to play around with the types of classifiers that we use here
@@ -53,26 +87,27 @@ eight <- rpart.control(cp=-1,maxdepth=3,minsplit=1) # 8-node tree
 four <- rpart.control(cp=-1,maxdepth=2,minsplit=1) # 4-node tree
 stump <- rpart.control(cp=-1,maxdepth=1,minsplit=1) # 2-node tree
 
-boost.stump <- ada(as.factor(SeriousDlqin2yrs)~., data=train.sub, 
-                       type = "real", control = stump, 
-                       test.x = test.sub[, -1], test.y = test.sub[, 1], iter = 200)
+boost.stump <- ada(as.factor(SeriousDlqin2yrs) ~ ., data = train.set, 
+                   type = "real", control = stump, iter = 200,
+                   test.x = test.set[ , -1], test.y = test.set[ , 1])
 
-boost.four <- ada(as.factor(SeriousDlqin2yrs)~., data=train.sub, 
-                       type = "real", control = four, 
-                       test.x = test.sub[, -1], test.y = test.sub[, 1], iter = 200)
+boost.four <- ada(as.factor(SeriousDlqin2yrs) ~ ., data = train.set, 
+                       type = "real", control = four, iter = 200,
+                       test.x = test.set[ , -1], test.y = test.set[ , 1])
 
-boost.eight <- ada(as.factor(SeriousDlqin2yrs)~., data=train.sub, 
-                 type = "real", control = eight, 
-                 test.x = test.sub[, -1], test.y = test.sub[, 1], iter = 200)
+boost.eight <- ada(as.factor(SeriousDlqin2yrs) ~ ., data = train.set, 
+                   type = "real", control = eight, iter = 200,
+                   test.x = test.set[ , -1], test.y = test.set[ , 1])
 
-boost.sixteen <- ada(as.factor(SeriousDlqin2yrs)~., data=train.sub, 
-                       type = "real", control = sixteen, 
-                       test.x = test.sub[, -1], test.y = test.sub[, 1], iter = 200)
+boost.sixteen <- ada(as.factor(SeriousDlqin2yrs) ~ ., data=train.set, 
+                     type = "real", control = sixteen, iter = 200,
+                     test.x = test.set[ , -1], test.y = test.set[ , 1])
+## performance plots
 varplot(boost.stump)
 plot(boost.stump)
 varplot(boost.four)
 plot(boost.four)
 varplot(boost.eight)
-plot(boot.eight)
+plot(boost.eight)
 varplot(boost.sixteen)
 plot(boost.sixteen)
